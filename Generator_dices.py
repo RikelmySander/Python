0912d6c04d96042c5afbf5d03e75ecae312c2ec9
RikelmySander <rikelmysander@gmail.com>
2024-11-05 20:04:55 -0300

    I've added a section for a quicker visualization of the results

--- Generator_dices.py
+++ Generator_dices.py
@@ -4,11 +4,13 @@ people fought, saying that the dice had to be played at the same time,
 with this program everything was solved
 """
 import random
-attacker = int(input("How many dice will the Attacker roll?: "))
-defensor = int(input("How many dice will the Defender roll?: "))
+attacker = int(input("How many dice will the Attacker roll? "))
+defensor = int(input("How many dice will the Defender roll? "))
 dices_Attacker = []
 dices_Defensor = []
 count = 0
+attacker_wins = 0
+defensor_wins = 0
 
 if attacker > 3 or defensor > 3:
     print("FAILED")
@@ -24,3 +26,10 @@ else:
     dices_Defensor.sort(reverse=True)
     print(f"The Attacker's dices are:: {dices_Attacker}")
     print(f"The Defensor's dices are:: {dices_Defensor}")
+
+for x,y in zip(dices_Attacker, dices_Defensor):
+    if x > y:
+        attacker_wins += 1
+    else:
+        defensor_wins += 1
+print(f"The attacker won: {attacker_wins}, and Defensor won: {defensor_wins}")
